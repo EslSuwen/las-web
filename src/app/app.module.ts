87bfcefd114f4160bc4dcbb5e3e900cbdbae69d4
pc-suwen <577014284@qq.com>
2020-09-30 09:31:26 +0800

    升级 Angular10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,15 +13,17 @@ import {NZ_I18N, zh_CN} from 'ng-zorro-antd/i18n';
 import {registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
-import {NzBadgeModule, NzDropDownModule, NzIconModule} from 'ng-zorro-antd';
+import {NzDropDownModule} from 'ng-zorro-antd/dropdown';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import {NzBadgeModule} from 'ng-zorro-antd/badge';
 import {ModalComponent} from './modal/modal.component';
 import {UpdatePasswordComponent} from './user-home/update-password/update-password.component';
 import {UserInfoComponent} from './user-home/user-info/user-info.component';
 import {ApplyComponent} from './user-home/apply/apply.component';
 import {RecordComponent} from './user-home/record/record.component';
 import {AngularMultiSelectModule} from 'angular2-multiselect-dropdown';
-import { AuditComponent } from './user-home/audit/audit.component';
-import { AuditRecordComponent } from './user-home/audit-record/audit-record.component';
+import {AuditComponent} from './user-home/audit/audit.component';
+import {AuditRecordComponent} from './user-home/audit-record/audit-record.component';
 
 registerLocaleData(zh);
 
